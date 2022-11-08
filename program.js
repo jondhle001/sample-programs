1a1c748b024dbd217a5e7a301fc77cbc6e043d4e
jondhle001 <jondhle.001@gmail.com>
2022-11-08 13:48:23 -0700

    small change

--- program.js
+++ program.js
@@ -4,7 +4,7 @@ const prompt = require("prompt-sync")();
 // take input from the user
 const guess = parseInt(prompt('Guess a number between 1 to 10:'));
 let number = Math.floor(Math.random() * 10)
-console.log("number you entered now ",guess)
+console.log("number you entered ",guess)
 console.log("correct number",number)
 // checking if number is negative
 if (number == guess) {
